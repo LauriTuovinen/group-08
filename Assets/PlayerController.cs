d2af0a708cd924bb13b0a2500db1614eb01283e0
Lauri Tuovinen <lare.tuovinen@gmail.com>
2024-10-30 22:08:31 +0200

    liikkuu ja hyppii

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float walkSpeed = 1;
+    [SerializeField] private float jumpForce = 45f;
+    [SerializeField] private Transform groundCheckpoint;
+    [SerializeField] private float groundCheckY = 0.2f; 
+    [SerializeField] private float groundCheckX = 0.5f; 
+    [SerializeField] private LayerMask whatIsGround;
+
+    private Rigidbody2D rb;
+    private float xAxis;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInputs();
+        Move();
+        Jump();
+    }
+
+    void GetInputs()
+    {
+        xAxis = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void Move() 
+    {
+        rb.linearVelocity = new Vector2(walkSpeed * xAxis, rb.linearVelocity.y);
+    }
+
+    public bool Grounded()
+    {
+        if(Physics2D.Raycast(groundCheckpoint.position, Vector2.down, groundCheckY, whatIsGround)
+            || Physics2D.Raycast(groundCheckpoint.position + new Vector3(groundCheckX, 0, 0), Vector2.down, groundCheckY, whatIsGround)
+            || Physics2D.Raycast(groundCheckpoint.position + new Vector3(-groundCheckX, 0, 0), Vector2.down, groundCheckY, whatIsGround))
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+    void Jump()
+    {
+        if (Input.GetButtonUp("Jump") && rb.linearVelocity.y > 0)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
+        }
+
+        if(Input.GetButtonDown("Jump") && Grounded())
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce);
+        }
+    }
+}
