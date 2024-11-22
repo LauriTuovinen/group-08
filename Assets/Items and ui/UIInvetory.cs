99842719eb877642bf19bb216a458996bd246129
Hajax8 <t2luha01@students.oamk.fi>
2024-11-22 17:06:31 +0200

    Inventory

---
+++ Assets/Items and ui/UIInvetory.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class UIInventory : MonoBehaviour
+{
+    [SerializeField] private UIInventoryItem itemPrefab;
+    [SerializeField] private RectTransform contentPanel;
+
+    List<UIInventoryItem> listOfUIItems = new List<UIInventoryItem>();
+
+    public void InitializeInventoryUI(int inventorysize)
+    {
+        for (int i = 0; i < inventorysize; i++)
+        {
+            UIInventoryItem uiItem =
+                Instantiate(itemPrefab, Vector3.zero, Quaternion.identity);
+            uiItem.transform.SetParent(contentPanel, false);
+            listOfUIItems.Add(uiItem);
+        }
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
